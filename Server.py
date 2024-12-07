3f02e4ba5dc9323a7a887fe3e0b35433e5b4b452
Haya-Alkaabi <202203340@stu.uob.edu.bh>
2024-12-07 11:56:16 +0300

    Update Server.py
    I updated the server file to have a good connection with the gui file and I added extra comments.

--- Server.py
+++ Server.py
@@ -1,27 +1,27 @@
 import socket
-import threading
+import threading 
 import json
 from newsapi import NewsApiClient
 
-key = '8f82fa538a7e499ebde5d4c1826c8686'
-newsapi = NewsApiClient(api_key=key)
+key = '8f82fa538a7e499ebde5d4c1826c8686' # my News API key
+newsapi = NewsApiClient(api_key=key) # create a News API object
 
-def HandlClient(clientSocket, clientAddress):
-    print(f"[NEW CONNECTION] {clientAddress} connected")
-    client_name = clientSocket.recv(1024).decode('utf-8')
+def HandlClient(clientSocket, clientAddress):  
+    print(f"[NEW CONNECTION] {clientAddress} connected") 
+    client_name = clientSocket.recv(1024).decode('utf-8') # receive client name
     print(f"[CLIENT NAME] {client_name} from {clientAddress}")
-
-    while True:
+     
+    while True: 
         try:
-            option = clientSocket.recv(1024).decode('utf-8')
-            type = clientSocket.recv(1024).decode('utf-8')
-            par = clientSocket.recv(1024).decode('utf-8')
+            option = clientSocket.recv(1024).decode('utf-8') # receive option
+            type = clientSocket.recv(1024).decode('utf-8') # receive type
+            par = clientSocket.recv(1024).decode('utf-8') # receive par
             print (f'The client chose {option} , with {type} as a request type and {par} as a parameter')
 
-            if not option:
+            if not option: 
                 break  # Client disconnected
 
-            if option == '1':
+            if option == '1': 
                 if type == '1.1':
                     news = newsapi.get_top_headlines(q=par, page_size=15)
                 elif type == '1.2':
@@ -31,14 +31,14 @@ def HandlClient(clientSocket, clientAddress):
                 elif type == '1.4':
                     news = newsapi.get_top_headlines(page_size=15)
 
-                response = json.dumps(news, ensure_ascii=False)  # Convert to JSON string
-                clientSocket.send(response.encode('utf-8'))  # Send as bytes
-                with open(f'{client_name}_top_headlines_{type}.json', 'w') as f:
-                    json.dump(news, f, indent=4)
+                response = json.dumps(news, ensure_ascii=False) # convert to json
+                clientSocket.send(response.encode('utf-8')) # Send response back to client
+                with open(f'{client_name}_top_headlines_{type}_groupB3.json', 'w') as f: # Save to file
+                    json.dump(news, f, indent=4) 
 
             if option == '2':
-                if type == '2.1':
-                    news = newsapi.get_sources(category=par)
+                if type == '2.1': 
+                    news = newsapi.get_sources(category=par) 
                 elif type == '2.2':
                     news = newsapi.get_sources(country=par)
                 elif type == '2.3':
@@ -46,25 +46,25 @@ def HandlClient(clientSocket, clientAddress):
                 elif type == '2.4':
                     news = newsapi.get_sources(page_size=15)
 
-                response = json.dumps(news, ensure_ascii=False)  # Convert to JSON string
-                clientSocket.send(response.encode('utf-8'))  # Send as bytes
-                with open(f'{client_name}_get_sources_{type}.json', 'w') as f:
-                    json.dump(news, f, indent=4)
+                response = json.dumps(news, ensure_ascii=False) # convert to json
+                clientSocket.send(response.encode('utf-8')) # Send response back to client
+                with open(f'{client_name}_get_sources_{type}_groupB3.json', 'w') as f: # Save to file
+                    json.dump(news, f, indent=4) 
 
-        except Exception as e:
-            print(f"[ERROR] {e}")
-            break
+        except Exception as e: # Handle any exceptions
+            print(f"[ERROR] {e}") 
+            break  
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-    server_socket.bind(("127.0.0.1",49999))
-    server_socket.listen(3)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket: # Create a socket object
+    server_socket.bind(("127.0.0.1",49999)) # Bind the socket to a address and port
+    server_socket.listen(3) # Listen for incoming connections(3 max)
 
-    print("# The server initiated and it is listening for client request #")
+    print("# The server initiated and it is listening for client request #")  
 
     # multithreading
-    while True:
-        clientSocket, clientAddress = server_socket.accept()
-        print("The connection established with ",clientAddress)
-        server_thread = threading.Thread(target = HandlClient, args = (clientSocket,clientAddress))
-        server_thread.start()
+    while True: # Continuously listen for incoming connections
+        clientSocket, clientAddress = server_socket.accept() # Accept the connection
+        print("The connection established with ",clientAddress) 
+        server_thread = threading.Thread(target = HandlClient, args = (clientSocket,clientAddress)) # Create a new thread for the client
+        server_thread.start() # Start the thread
