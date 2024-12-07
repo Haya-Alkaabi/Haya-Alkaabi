9579ad2a07a8cccaf9727eec31d0cc1f6db21db2
Muneera-Almuhaiza <202206145@stu.uob.edu.bh>
2024-12-07 11:09:42 +0000

    Update Client.py
    This file (Client.py) contains the client tasks only without GUI

--- Client.py
+++ Client.py
@@ -1,36 +1,36 @@
 import socket
 
 n = input("Welcome! Press Enter")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect(("127.0.0.1", 49999))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket: 
+        client_socket.connect(("127.0.0.1", 49999)) 
 
         # ask for user name and send it to the server
-        client_name = input("Enter your name: ")
-        client_socket.send(client_name.encode('utf-8'))
-        while True:
+        client_name = input("\nEnter your name: ") 
+        client_socket.send(client_name.encode('utf-8')) 
 
-            print("\n Menu options:")
+        while True: 
+            print("\nMenu options:")
             print("1. Search headlines")
             print("2. List of Sources")
             print("3. Quit")
 
-            option = input("Enter the menu option:")
+            option = input("\nEnter the menu option:") 
 
             if option == '1':
-                client_socket.send(option.encode('utf-8'))
+                client_socket.send(option.encode('utf-8'))   
                 print("1.1 Search for keywords")
                 print("1.2 Search by category")
                 print("1.3 Search by country")
                 print("1.4 List all new headlines")
                 print("1.5 Back to the main menu")
 
-                while True:
-                    service = input("Enter the option number:")
+                while True: 
+                    service = input("\nEnter the option number:") 
                     if service == '1.1':
-                        client_socket.send(service.encode('utf-8'))
+                        client_socket.send(service.encode('utf-8'))  
                         print("Search for keywords")
-                        par= input("Enter the keyword: ")
-                        client_socket.send(par.encode('utf-8'))
+                        par= input("Enter the keyword: ") 
+                        client_socket.send(par.encode('utf-8')) 
                         type_request = 'keywords'
                     elif service == '1.2':
                         client_socket.send(service.encode('utf-8'))
@@ -53,13 +53,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
                     elif service == '1.5':
                         client_socket.send(service.encode('utf-8'))
                         break
-                    '''
-                    client_socket.send('1'.encode('utf-8'))
-                    client_socket.send(type_request.encode('utf-8'))
-                    client_socket.send(par.encode('utf-8'))
-                    '''
-                    response = client_socket.recv(4069).decode('utf-8')
-                    print(response)
+                       
+                    response = client_socket.recv(4069).decode('utf-8')    
+                    print(response) 
 
             if option == '2':
                 print("2.1 Search by category")
@@ -69,12 +65,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
                 print("2.5 Back to the main menu")
 
                 while True:
-                    service = input("Enter the option number:")
+                    service = input("\nEnter the option number:")
                     if service == '2.1':
-                        client_socket.send(service.encode('utf-8'))
+                        client_socket.send(service.encode('utf-8')) 
                         print("business, general, health, science, sports, technology")
                         par = input("Choose a category: ")
-                        client_socket.send(par.encode('utf-8'))
+                        client_socket.send(par.encode('utf-8')) 
                         type_request = 'category'
                     elif service == '2.2':
                         client_socket.send(service.encode('utf-8'))
@@ -97,6 +93,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
                     elif service == '2.5':
                         break
 
-            if option == '3':
+            if option == '3':    
                 print("Good bye!")
                 exit()
